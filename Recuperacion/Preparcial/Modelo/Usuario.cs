c88fbae81358e58c4277a834bc6d14d487137be4
Roberto <robertocabezas880@gmail.com>
2020-05-31 18:13:20 -0600

    Add: Recuperacion

---
+++ Recuperacion/Preparcial/Modelo/Usuario.cs
@@ -0,0 +1,21 @@
+﻿namespace Preparcial.Modelo
+{
+    public class Usuario
+    {  //Corrección: Se eliminaron las propiedades setters por ser innecesarias
+        public string IdUsuario { get;  }
+
+        public string NombreUsuario { get;  }
+
+        public string Contrasena { get;  }
+
+        public bool Admin { get;  }
+
+        public Usuario(string idUsuario, string nombreUsuario, string contrasenia, bool admin)
+        {
+            IdUsuario = idUsuario;
+            NombreUsuario = nombreUsuario;
+            Contrasena = contrasenia;
+            Admin = admin;
+        }
+    }
+}
